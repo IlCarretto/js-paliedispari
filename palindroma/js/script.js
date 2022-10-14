508868c3ee34371d5e07adb01250154ce8675fdd
IlCarretto <carlorebuffat412@gmail.com>
2022-10-14 16:43:08 +0200

    function written

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,10 +3,8 @@
 // Creare una funzione per capire se la parola inserita è palindroma.
 
 let userWord = prompt("Inserisci una parola palindroma")
-console.log(userWord);
-const userLetters = userWord.split("");
-console.log(userLetters);
-
+const userLetters = userWord.split(""); 
+console.log(userLetters, userLetters.length);
 
 // FUNCTIONS
 
@@ -15,7 +13,16 @@ console.log(userLetters);
  * @param {string}; Stringa da controllare
  * @returns {boolean}; valore booleano, true se è palindroma, false se non è palindroma
  */
-function elementIsPalindroma (wordToCheck) {
+function elementIsPalindroma (wordToCheck, wordReversed) {
     let wordWritten = false;
-
+    for (let i = userLetters.length; i = 0; i--) {
+        wordReversed.push(userLetters.length[i]);
+        if (wordReversed === wordToCheck) {
+            wordWritten = true;
+        }
+    }
+    return wordWritten;
+    console.log(wordReversed);
 }
+
+console.log(elementIsPalindroma(userWord));
