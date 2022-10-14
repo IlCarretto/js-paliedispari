6a712d22c4ab75e9030636642bd289fb73fbf3bd
IlCarretto <carlorebuffat412@gmail.com>
2022-10-14 16:00:59 +0200

    divided userWord in letters

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,21 @@
-alert("we")
+// **Palindroma**
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+let userWord = prompt("Inserisci una parola palindroma")
+console.log(userWord);
+const userLetters = userWord.split("");
+console.log(userLetters);
+
+
+// FUNCTIONS
+
+/**
+ * Description: Funzione che controlla se la parola inserita dall'utente è palindroma 
+ * @param {string}; Stringa da controllare
+ * @returns {boolean}; valore booleano, true se è palindroma, false se non è palindroma
+ */
+function elementIsPalindroma (wordToCheck) {
+    let wordWritten = false;
+
+}
